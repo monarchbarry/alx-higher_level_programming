61d2233edc3d99410f4bf3643e3f1b7bacf33973
Monarch <ayobarium@gmail.com>
2024-04-16 09:59:50 -0700

    pycodestyle fix

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """script to list cities"""
-
 from sqlalchemy import Column, Integer, String, ForeignKey
 from relationship_state import Base
+
 
 class City(Base):
     """Class representing the states table"""
     __tablename__ = 'cities'
-
     id = Column(Integer, nullable=False, primary_key=True,
                 autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
